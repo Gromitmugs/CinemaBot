c107f5f031191ac466b7832c989455dc13d5d099
Gromitmugs <jediacp2000@gmail.com>
2022-12-07 16:11:03 +0700

    camera_node

---
+++ scripts/aruco.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+from imutils.video import VideoStream
+import sys
+
+class ArUco:
+    def detectAruco(frame, marker_size=6, total_markers=250):
+
+        arucoType = f"DICT_{marker_size}X{marker_size}_{total_markers}"
+
+        # define names of each possible ArUCo tag OpenCV supports
+        ARUCO_DICT = {
+            "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+            "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+            "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+            "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+            "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+            "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+            "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+            "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+            "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+            "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+            "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+            "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+            "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+            "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+            "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+            "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+            "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+            "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+            "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+            "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+            "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+        }
+
+        # check if the input ArUCo tag exists and supported by openCV
+        if arucoType not in ARUCO_DICT:
+            print(f"[INFO] ArUCo tag of {arucoType} is not supported")
+            sys.exit()
+
+        # load the ArUCo dictionary and grab the ArUCo parameters
+        arucoDict = aruco.Dictionary_get(ARUCO_DICT[arucoType])
+        arucoParams = aruco.DetectorParameters_create()
+
+        # detect ArUCo markers in the input frame
+        corners, ids, _ = aruco.detectMarkers(
+            frame, arucoDict, parameters=arucoParams)
+
+        return corners, ids
---
+++ scripts/camera_node.py
@@ -0,0 +1,39 @@
+from aruco import ArUco
+import cv2
+import rospy
+from geometry_msgs.msg import Pose
+from cinema_bot.srv import GuestAPI
+
+
+
+def camera_node(path_type): # node name
+    pub = rospy.Publisher('/SeatLocation', Pose, queue_size=10)
+    rospy.init_node('camera_node', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    cap = cv2.VideoCapture(0)
+
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (1280, 720))
+        corners, ids = ArUco.detectAruco(
+            frame, marker_size=5, total_markers=50)
+        
+        if len(ids) > 0: #aruco detected
+            pose_response = callGuestAPI(str(ids[0]))
+            pub.publish(pose_response)
+
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+
+
+def callGuestAPI(id):
+    try:
+            matrix = rospy.ServiceProxy('GuestAPI', GuestAPI)
+            response = GuestAPI(id)
+            return response
+    except rospy.ServiceException as e:
+            print("GuestAPI call failed: %s"%e)
+
